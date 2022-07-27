0a20c7a9100ed0fde6baaef880137224569dc3ee
sahriyar eshar <princeeshar@gmail.com>
2022-07-27 23:10:17 +0600

    project_task

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div class="background">Developed By Sahriyar...</div>
+    <v-main class="d-flex justify-center align-center">
+      <v-col cols="10" lg="4" class="mx-auto">
+        <v-card class="pa-4">
+          <div class="text-center">
+            <v-avatar size="100" color="indigo lighten-4">
+              <v-icon size="40" color="indigo">mdi-account</v-icon>
+            </v-avatar>
+            <h2 class="indigo--text">Login Page</h2>
+          </div>
+          <v-form @submit.prevent="submitHandler" ref="form">
+            <v-card-text>
+              <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                type="email"
+                label="Email"
+                placeholder="Email"
+                prepend-inner-icon="mdi-account"
+              />
+              <v-text-field
+                    v-model="password"
+                    :rules="passwordRules"
+                :type="password?'password':'text'"
+                label="Password"
+                placeholder="Password"
+                prepend-inner-icon="mdi-key"
+                :append-icon="passwordShow ? 'mdi-eye':'mdi-eye-off'"
+                @click:append="passwordShow = !passwordShow"
+              />
+              <v-switch label="Remember me" color="indigo"></v-switch>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn :loading="loading" type="submit" color="indigo">
+                <span class="white--text px-8">Login</span>
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-main>
+    <v-snackbar top color="green" v-model="snackbar">
+      Login successful..!!
+    </v-snackbar>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+
+  data: () => ({
+    loading:false,
+    snackbar:false,
+    passwordShow:false,
+     password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be 6 characters or more!',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+  }),
+  methods:{
+    submitHandler(){
+      if (this.$refs.form.validate()){
+        this.loading = true
+       setTimeout(()=>{
+        this.loading = false
+        this.snackbar = true
+       }, 3000)
+      }
+       
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .background{
+    background-image: url(./assets/maple-1079235__340.jpg) !important ;
+    height: 300px;
+    width: 100%;
+    display: block;
+    position: absolute;
+    top: 0;
+    background-size: cover;
+  }
 </style>
